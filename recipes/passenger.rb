926316a15953bf7ad2022086312ef9beffc5ac45
Chris Fordham <chris@xhost.com.au>
2014-12-31 09:38:00 +1100

    C: Prefer single-quoted strings when you don't need string interpolation or special symbols.

--- recipes/passenger.rb
+++ recipes/passenger.rb
@@ -23,7 +23,7 @@ log 'Generating server SSL cert' do
 end
 
 # The puppet agent must be run at least once to generate the server's SSL cert
-execute "puppet agent --waitforcert 60 -t" do
+execute 'puppet agent --waitforcert 60 -t' do
   not_if { File.exist?("/var/lib/puppet/ssl/certs/#{node['puppet']['client_conf']['main']['server']}") }
   returns [0, 2]
   timeout 120
