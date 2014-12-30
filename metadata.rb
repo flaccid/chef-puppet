65195f6007d169c31c6f8e0f1cc57494b6307a10
Chris Fordham <chris@xhost.com.au>
2014-12-30 15:13:13 +1100

    C: Prefer single-quoted strings when you don't need string interpolation or special symbols.

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name              "puppet"
-maintainer        "Fletcher Nichol"
-maintainer_email  "fnichol@nichol.ca"
-license           "Apache 2.0"
-description       "Installs and manages a Puppet Master service. No, really."
+name              'puppet'
+maintainer        'Fletcher Nichol'
+maintainer_email  'fnichol@nichol.ca'
+license           'Apache 2.0'
+description       'Installs and manages a Puppet Master service. No, really.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.2.1"
+version           '0.2.1'
 
-supports "ubuntu"
+supports 'ubuntu'
 
-depends "apt"
-depends "yum"
+depends 'apt'
+depends 'yum'
