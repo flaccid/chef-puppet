385c3e9ab3ac1e0b3a4c083982f218a6e2543280
Chris Fordham <chris@xhost.com.au>
2015-01-02 12:29:04 +1100

    Restart the pe-pupet service for autosign.conf.

--- recipes/master_pe_services.rb
+++ recipes/master_pe_services.rb
@@ -1,8 +1,10 @@
 #
 # Cookbook Name:: puppet
-# Recipe:: whitelist
+# Recipe:: master_pe_services
 #
-# Copyright 2014, Sean Carolan
+# Copyright 2014-2015, Chris Fordham
+# Copyright 2014-2015, Fletcher Nichol
+#
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,15 +19,16 @@
 # limitations under the License.
 #
 
-include_recipe 'master_pe_services' if node['puppet']['edition'] == 'pe'
+# this recipe (currently) should not be run without the
+# master_pe recipe in the run_list or pe-puppet already installed
 
-# Configures the Puppet server's whitelist, which allows new clients
-# to auto-register.
-template "#{node['puppet']['confdir']}/autosign.conf" do
-  source 'autosign.conf.erb'
-  mode '0644'
-  variables(
-    whitelist: node['puppet']['autosign']['whitelist']
-  )
-  notifies :restart, 'service[pe-puppet]', :delayed if node['puppet']['edition'] == 'pe'
-end
+service 'pe-activemq'
+service 'pe-console-services'
+service 'pe-httpd'
+service 'pe-mcollective'
+service 'pe-memcached'
+service 'pe-postgresql'
+service 'pe-puppet'
+service 'pe-puppet-dashboard-workers'
+service 'pe-puppetdb'
+service 'pe-puppetserver'
